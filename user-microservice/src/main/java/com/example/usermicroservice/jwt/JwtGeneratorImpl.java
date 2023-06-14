340f27e27ce65793c1d15acf07b4ac0571424fd5
ImaniLeemans <455181@student.fontys.nl>
2023-06-14 11:26:37 +0200

    Send user id in JWTToken.

--- user-microservice/src/main/java/com/example/usermicroservice/jwt/JwtGeneratorImpl.java
+++ user-microservice/src/main/java/com/example/usermicroservice/jwt/JwtGeneratorImpl.java
@@ -16,13 +16,45 @@ public class JwtGeneratorImpl implements JwtGeneratorInterface{
     private String secret;
     @Value("${app.jwttoken.message}")
     private String message;
-    @Override
+//    @Override
+//    public Map<String, String> generateToken(User user) {
+//        String jwtToken="";
+//        jwtToken = Jwts.builder().setSubject(user.getUserName()).setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, "secret").compact();
+//        Map<String, String> jwtTokenGen = new HashMap<>();
+//        jwtTokenGen.put("token", jwtToken);
+//        jwtTokenGen.put("message", message);
+//        return jwtTokenGen;
+//    }
+
+//    @Override
+//    public Map<String, String> generateToken(User user) {
+//        String jwtToken="";
+//        jwtToken = Jwts.builder()
+//        .setSubject(user.getUserName())
+//        .setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, "secret")
+//                .claim("userId", user.getId())
+//       .compact();
+//
+//
+//        Map<String, String> jwtTokenGen = new HashMap<>();
+//        jwtTokenGen.put("token", jwtToken);
+//        jwtTokenGen.put("message", message);
+//        return jwtTokenGen;
+//    }
+
+        @Override
     public Map<String, String> generateToken(User user) {
-        String jwtToken="";
-        jwtToken = Jwts.builder().setSubject(user.getUserName()).setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, "secret").compact();
+        String jwtToken = Jwts.builder()
+                .setSubject(user.getUserName())
+                .claim("userId", String.valueOf(user.getId())) // Convert user ID to string
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secret")
+                .compact();
+
         Map<String, String> jwtTokenGen = new HashMap<>();
         jwtTokenGen.put("token", jwtToken);
         jwtTokenGen.put("message", message);
         return jwtTokenGen;
     }
+
 }
